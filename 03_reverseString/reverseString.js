ec79a4e8e669fa8567da862738382a1bb7102e4e
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2024-11-18 18:41:06 -0500

    Return its input reversed
    Split the string in an array of characters. Used a loop to join the
    characters to the string in reversed order.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(originalString) {
+    let reversedString = "";
+    let stringArray = originalString.split("");
+    for (let i = 0; i < stringArray.length; i++) {
+        reversedString += stringArray[stringArray.length - (i + 1)];
+    }
+    return reversedString
 };
 
 // Do not edit below this line
