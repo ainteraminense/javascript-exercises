e2001a523f209449770134e3d69719648af839f4
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2024-11-18 18:47:44 -0500

    Refactor after taking a peek at the solution folder

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,6 @@
 const reverseString = function(originalString) {
-    let reversedString = "";
-    let stringArray = originalString.split("");
-    for (let i = 0; i < stringArray.length; i++) {
-        reversedString += stringArray[stringArray.length - (i + 1)];
-    }
-    return reversedString
+    let  reversedString = originalString.split("").reverse().join("");
+    return reversedString;
 };
 
 // Do not edit below this line
