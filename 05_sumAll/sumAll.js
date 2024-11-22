3b4c0c613e23a4661b97319478d99363006df23e
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2024-11-22 11:07:19 -0500

    Return sum of integers between 2 positive integers
    Took two numbers and calculated the sum of every integers from one
    number to the other.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1 , num2) {
+    let sum = 0;
+    if (
+      !Number.isInteger(num1) ||
+      !Number.isInteger(num2) ||
+      num1 < 0 ||
+      num2 < 0
+      ) {
+        return "ERROR";
+    }
+    const MIN_VALUE = Math.min(num1, num2);
+    const MAX_VALUE = Math.max(num1, num2);
+    for (let i = MIN_VALUE; i <= MAX_VALUE; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
