811b8352c40114219c872ee2fc18c48f4d2448ce
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2024-11-22 09:53:23 -0500

    Remove multiple arguments from an array
    Checked a list of arguments and checked if there was an instance or
    multiple instances of that list item in the array and removed them from it and
    returned it.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (i of args) {
+        while(array.indexOf(i) != -1) {
+            array.splice(array.indexOf(i), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
