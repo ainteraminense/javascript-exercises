fee2efb2b4d3e01814b2b8ebbcc94803a7f4fb88
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2024-11-22 11:47:12 -0500

    Convert temperature between fahrenheit and celsius
    Converted from fahrenheit to celsius and round to 1 decimal place. Convert
    from celsius to fahrenheit and round to 1 decimal place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32) * 5 / 9;
+  tempC = Number(tempC.toFixed(1));
+  return tempC 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC * 9 / 5) + 32;
+  tempF = Number(tempF.toFixed(1));
+  return tempF;
 };
 
 // Do not edit below this line
