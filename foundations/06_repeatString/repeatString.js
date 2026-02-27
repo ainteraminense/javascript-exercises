ec1cc54983cc0e13a56f03fbee335ab6e99a304d
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-02-27 15:51:58 -0500

    concatenate the number of times chosen and return it

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, repetition) {
+    // if repetition is negative return 'ERROR'
+    // use loop to repeat strings
+    let result = ''
+    if (repetition < 0) {
+        return 'ERROR';
+    } 
+    for (i = 0; i < repetition; i ++) {
+        result += text;
+    }
+    return result;
 };
 
 // Do not edit below this line
