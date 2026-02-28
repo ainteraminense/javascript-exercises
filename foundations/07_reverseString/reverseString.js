3bbb71847cee12f1150461c08c4d9f74c2819cb8
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-02-28 15:27:57 -0500

    Write function to revert a string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reversedString = string.split("");
+    reversedString = reversedString.reverse();
+    reversedString = reversedString.join("");
+    return reversedString
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
