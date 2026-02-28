193e2e6f7139bbcdbde3a75cd456b1ecef2a3e9b
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-02-28 16:15:01 -0500

    Practice the cleaner provided solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,13 +1,16 @@
 const removeFromArray = function(arr, ...values) {
-    for (let value of values) {
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] === value) {
-                arr.splice(i, 1);
-                i--;
-            }
-        }
-    }
-    return arr;
+    // for (let value of values) {
+    //     for (let i = 0; i < arr.length; i++) {
+    //         if (arr[i] === value) {
+    //             arr.splice(i, 1);
+    //             i--;
+    //         }
+    //     }
+    // }
+    // return arr;
+    newArr = [];
+    newArr = arr.filter((arrValue) => !values.includes(arrValue));
+    return newArr;
 };
 
 // Do not edit below this line
