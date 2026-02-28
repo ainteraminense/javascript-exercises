ffcb468ba4369b187538ea76be894228006f4baf
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-02-28 16:08:40 -0500

    Remove all items of the array from the entered parameters

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+    for (let value of values) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === value) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
