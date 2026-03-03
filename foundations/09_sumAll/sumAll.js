c5cba19c5cee25b9e4f5a2aeb99d5a01b075e3f7
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-03-03 18:53:46 -0500

    Change design after taking peak at the solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,22 +1,22 @@
-const sumAll = function(num1, num2) {
-    let sum = 0;
-    if (num1 < 0 || 
-        num2 < 0 ||
-        !Number.isInteger(num1) ||
-        !Number.isInteger(num2)) {
+const sumAll = function(min, max) {
+    const temp = max;
+    let total = 0;
+    if (min < 0 || 
+        max < 0 ||
+        !Number.isInteger(min) ||
+        !Number.isInteger(max)) {
         return "ERROR";
     }
-    if (num1 > num2) {
-        for (let i = num2; i <= num1; i++) {
-            sum += i;
-        }
-    } else {
-        for (let i = num1; i <= num2; i++) {
-            sum += i;
-        }
+    if (min > max) {
+        max = min;
+        min = temp;
+
     }
-    return sum;
-};
+    for (let i = min; i <= max; i++) {
+        total += i;
+    }
+    return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
