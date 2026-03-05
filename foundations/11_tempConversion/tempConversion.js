e675989b656cde29adcd429ea7263db950a0cae5
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-03-05 18:19:20 -0500

    Convert temperature between celsius and fahrenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  const temperatureInCelsius = (temperatureInFahrenheit - 32) * 5 / 9;
+  return Math.round(( temperatureInCelsius * 10 )) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  const temperatureInFahrenheit = temperatureInCelsius * 9 / 5 + 32;
+  return Math.round( (temperatureInFahrenheit * 10)) / 10;
 };
 
 // Do not edit below this line
