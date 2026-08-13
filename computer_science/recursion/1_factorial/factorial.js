3b5b3b6fc339c1eee884a02762a991ccf9789d4a
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-08-13 12:08:45 -0400

    Take a non-negative integer, and returns the product of all positive integers less than or equal to the input integer

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function(number) {
+    if (number < 0 || !Number.isInteger(number)) {
+        return undefined;
+    } else if (number === 0) {
+        return 1;
+    } else {
+        return number * factorial (number - 1);
+    }
 };
 
 // Do not edit below this line
