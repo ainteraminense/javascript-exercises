55c2e16d904aa08121a974d2e8ec416c794d7e34
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-08-14 10:56:22 -0400

    Check if object contains value regardless of granularity of search

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,8 @@
-const contains = function() {
-  
-};
+const contains = function(object, value) {
+    const values = Object.values(object);
+    const nestedObject = values.filter( (obj) => typeof obj === "object" && obj !== null);
+    return values.includes(value) ? true : nestedObject.some((nestedValue) => contains(nestedValue, value));
+}
   
 // Do not edit below this line
 module.exports = contains;
