01b38eb3ad23658447b59e2a8f2561a4129ec7de
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-08-15 13:52:49 -0400

    Give all permutation from an empty array or an array from a consecutive numbers starting from 1

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,20 @@
-const permutations = function() {
-  
+const permutations = function(array) {
+  if (array.length === 0 || array.length === 1) {
+    return [array];
+  } else {
+    const result = [];
+    const curr = array[0];
+    const mutations = permutations(array.slice(1));
+
+    for (mutation of mutations) {
+        for(let i = 0; i <= mutation.length; i++) {
+            const left = mutation.slice(0, i);
+            const right = mutation.slice(i);
+            result.push([...left, curr, ...right]);
+        }
+    }
+    return result;
+  }
 };
   
 // Do not edit below this line
