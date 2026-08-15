522e7f0867c0d92ee68b7354a71130a0263fb54e
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-08-15 14:43:32 -0400

    create a recursive function, pascal - that will take an input n and output the nth pascal's row as an array of numbers

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,20 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+  if (n === 1) {
+    return [1];
+  } else {
+    const previousRow = pascal(n-1);
+    const curr = [];
+    for (let i = 0; i <= previousRow.length; i++) {
+        if(i === 0) {
+            curr.push(0 + previousRow[0]);
+        } else if (i === previousRow.length) {
+            curr.push (previousRow[i - 1] + 0)
+        } else {
+            curr.push(previousRow[i - 1] + previousRow[i])
+        }
+    }
+    return curr;
+  }
 };
   
 // Do not edit below this line
